48d37bdc69fac40609b19734a8291c993e59d323
Yaroslav Lyulka <63869333+lyulka-yar@users.noreply.github.com>
2020-12-13 14:38:47 +0200

    only app file

--- react_october/src/App.js
+++ react_october/src/App.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
-
+import React, {
+   Component
+} from 'react';
+import './App.css'
 class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
 
+   arr=[1,-1,100,-100];
+   state={result:0};
+
+   multipli = (number) => {
+      let {result} = this.state;
+      const value = result + number
+      value > 0
+          ? this.setState({result: value})
+          : this.setState({result: 0})
+
+   }
+   clear = () => {
+      this.setState({result:0})
+   }
+
+   render() {
+      const {result}=this.state;
+
+
+
+      return (
+
+         <div className={'calc'}>
+             <div className={'out'}>{result}</div>
+            {
+              <div className={'buttons'}> {this.arr.map((btn)=><button onClick={()=>this.multipli(btn)} className={'btn'}>{btn}</button>)}
+              <button onClick={()=>this.clear()} className={'btn clear'}>clear</button></div>
+              
+            }
+         </div>
+      );
+   }
+}
 export default App;
