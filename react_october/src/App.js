aad3d20ac6b92af9e9151ff4bb428ba8312b67be
Yaroslav Lyulka <63869333+lyulka-yar@users.noreply.github.com>
2020-12-15 17:15:33 +0200

    input

--- react_october/src/App.js
+++ react_october/src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 class App extends Component {
 
    arr=[1,-1,100,-100];
-   state={result:0};
+   state={result:0,inputChange:''};
 
    multipli = (number) => {
       let {result} = this.state;
@@ -13,16 +13,30 @@ class App extends Component {
       value > 0
           ? this.setState({result: value})
           : this.setState({result: 0})
-
    }
    clear = () => {
       this.setState({result:0})
    }
 
+
+   changeInp = () => {
+      const {result, inputChange} = this.state
+      const num = +inputChange;
+      if (!isNaN(num)) {
+         const value = result + num
+         value > 0
+             ? this.setState({result: value})
+             : this.setState({result: 0})
+      }
+      this.setState({inputChange:''})
+   }
+
+   addNum = (value) => {
+     this.setState({inputChange:value})
+   }
+
    render() {
       const {result}=this.state;
-
-
 
       return (
 
@@ -30,8 +44,10 @@ class App extends Component {
              <div className={'out'}>{result}</div>
             {
               <div className={'buttons'}> {this.arr.map((btn)=><button onClick={()=>this.multipli(btn)} className={'btn'}>{btn}</button>)}
+               <input onChange={event => this.addNum(event.target.value)} type="text" name="text" id="inp"/>
+               <button onClick={this.changeInp} className={'insertion'}>input</button>
               <button onClick={()=>this.clear()} className={'btn clear'}>clear</button></div>
-              
+
             }
          </div>
       );
