2ab4b1aa566b62ed33476925dd3d23cdf39df841
lyulka-yar <yaroslavliulka@gmail.com>
2020-12-22 21:03:20 +0200

    calc with useState

--- react_october/src/App.js
+++ react_october/src/App.js
@@ -1,13 +1,21 @@
-import React, {Component} from 'react';
+import React, {useState} from "react";
+import './App.css'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+export default function () {
+    const arr = [1, -1, 100,-100];
+ const [counter,setCounter] = useState(0);
+ const editCounter = (value) => {
+     const num = value + counter
+     num >= 0
+     ? setCounter(num)
+     : setCounter(0)
+ }
+    return (
+        <div>
+            {counter}
+            {
+               arr.map(value => <button onClick={()=>editCounter(value)}>{value}</button>)
+            }
+        </div>
+    )
 }
-
-export default App;
