a8d930637daf048a06e53c817609171e8cbc0904
Yaroslav Lyulka <63869333+lyulka-yar@users.noreply.github.com>
2020-12-23 23:56:35 +0200

    add input && reset

--- react_october/src/App.js
+++ react_october/src/App.js
@@ -1,21 +1,29 @@
 import React, {useState} from "react";
 import './App.css'
 
-export default function () {
+export default function App () {
     const arr = [1, -1, 100,-100];
- const [counter,setCounter] = useState(0);
+ const [counter,setCounter] = useState('');
  const editCounter = (value) => {
      const num = value + counter
      num >= 0
      ? setCounter(num)
      : setCounter(0)
  }
+    const resetCounter = () => {
+        setCounter(0)
+    }
+
+
     return (
-        <div>
-            {counter}
-            {
-               arr.map(value => <button onClick={()=>editCounter(value)}>{value}</button>)
-            }
+        <div className='wrapper'>
+            <span className='output'> {counter}</span>
+            <div className='input-wrap'><input onInput={(e)=>setCounter(parseInt(e.currentTarget.value))} type="text"/>
+            </div>
+            <div className='btn'> {
+                arr.map(value => <button onClick={() => editCounter(value)}>{value}</button>)
+            } </div>
+            <button className='reset' onClick={() => resetCounter()}>reset</button>
         </div>
     )
 }
